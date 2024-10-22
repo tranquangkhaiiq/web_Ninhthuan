84fc0b0f12be79897fd0a00c23f5dee8b330ef38
Tran Quang Khai <tranquangkhaiiq@gmail.com>
2024-10-22 16:26:38 +0700

    ngay 22thang 10
    Sua code New_Dao

--- WebViecLammoi/DAO/New_Dao.cs
+++ WebViecLammoi/DAO/New_Dao.cs
@@ -116,37 +116,8 @@ namespace WebViecLammoi.DAO
         }
         //
         public static List<New_small> Get_NewQCslideNOisActive(VLDB db, int skip, int take)
-        {//chỉ có 10 tin nổi bật
-            //var dataphien = db.News.Where(kh => kh.CategoryId == 3303 && kh.isActive == true).Count();
-            //var model = new List<New_small>();
-            //if (skip == 0 && take == 1)
-            //{//chọn "Thông báo và kết quả Phiên giao dịch việc làm: 3303"
-            //    if (dataphien > 0)
-            //    {
-            //        model = model_NewsSlide.Where(kh => kh.CategoryId == 3303).ToList();
-            //    }
-            //    else
-            //    {
-            //        model = model_NewsSlide.OrderBy(kh => kh.CategoryId)
-            //                        .ThenByDescending(kh => kh.Createdate)
-            //                        .Take(1)
-            //                        .ToList();
-            //    }
-            //}
-            //else if (skip == 0 && take > 1)
-            //{// lấy theo ngày mới
-            //    model = model_NewsSlide.OrderByDescending(kh => kh.Createdate)
-            //                            .Take(take)
-            //                            .ToList();
-            //}
-            //else if (skip > 0)
-            //{// ưu tiên "Đào tạo, dạy nghề: 3680"
-            //    model = model_NewsSlide.Where(kh => kh.CategoryId != 3303)
-            //            .OrderBy(kh => kh.CategoryId).ThenByDescending(kh => kh.Createdate)
-            //            .Skip(skip)
-            //            .Take(take)
-            //            .ToList();
-            //}
+        {//chỉ có 10 tin nổi bật quan ly boi proc "GetListNew_Slide_Khai"
+
             ////*****isActive =1 and Hotsite =1 order by Createdate2 desc
             var model = new List<New_small>();
             if (skip == 0 && take == 1)
@@ -157,7 +128,7 @@ namespace WebViecLammoi.DAO
             }
             else if (skip == 0 && take > 1)
             {
-                model  = model_NewsSlide.Take(1).Take(take).ToList();
+                model  = model_NewsSlide.Take(take).ToList();
             }
             else if (skip > 0)
             {
